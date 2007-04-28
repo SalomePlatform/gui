f7afb3b32f5e0d7b962928f436b4ff1c4209529e
stv <stv@opencascade.com>
2007-04-28 11:19:36 +0000

    *** empty log message ***

--- src/SUIT/SUIT_ViewWindow.cxx
+++ src/SUIT/SUIT_ViewWindow.cxx
@@ -1,17 +1,17 @@
 // Copyright (C) 2005  OPEN CASCADE, CEA/DEN, EDF R&D, PRINCIPIA R&D
-// 
+//
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either 
+// License as published by the Free Software Foundation; either
 // version 2.1 of the License.
-// 
-// This library is distributed in the hope that it will be useful 
-// but WITHOUT ANY WARRANTY; without even the implied warranty of 
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+//
+// This library is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public  
-// License along with this library; if not, write to the Free Software 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 //
 // See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
@@ -90,7 +90,7 @@ QImage SUIT_ViewWindow::dumpView()
 bool SUIT_ViewWindow::dumpViewToFormat( const QImage& img, const QString& fileName, const QString& format )
 {
   if( img.isNull() )
-    return false; 
+    return false;
 
   QString fmt = format;
   if( fmt.isEmpty() )
@@ -124,9 +124,9 @@ void SUIT_ViewWindow::setDestructiveClose( const bool on )
 
 /*! Close event \a theEvent.
 */
-void SUIT_ViewWindow::closeEvent(QCloseEvent* theEvent)
+void SUIT_ViewWindow::closeEvent( QCloseEvent* e )
 {
-//  QMainWindow::closeEvent( theEvent );
+  e->ignore();
   emit closing( this );
 }
 
@@ -209,7 +209,7 @@ QString   SUIT_ViewWindow::getVisualParameters()
 /*!
   Sets visual parameters of window by its string representation
   \param parameters - string with visual parameters
-*/ 
+*/
 void SUIT_ViewWindow::setVisualParameters( const QString& parameters )
 {
 }

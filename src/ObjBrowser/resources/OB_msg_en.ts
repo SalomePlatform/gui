c5da15fe635724382e5604485c0413e626a52276
vsr <vsr@opencascade.com>
2007-08-03 12:47:58 +0000

    *** empty log message ***

--- src/ObjBrowser/resources/OB_msg_en.ts
+++ src/ObjBrowser/resources/OB_msg_en.ts
@@ -1,9 +1,13 @@
 <!DOCTYPE TS><TS>
 <context>
-    <name>@default</name>
+    <name>OB_Browser</name>
     <message>
         <source>MEN_EXPAND_ALL</source>
         <translation>Expand All</translation>
+    </message>
+    <message>
+        <source>MEN_COLLAPSE_ALL</source>
+        <translation>Collapse All</translation>
     </message>
 </context>
 </TS>

806ca83620770fb16609e6b67190c5cd352b92b6
vsr <vsr@opencascade.com>
2007-08-21 12:28:36 +0000

    *** empty log message ***

--- src/TOOLSGUI/resources/ToolsGUI_icons.ts
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE TS><TS>
-<context>
-    <name>ToolsGUI_CatalogGeneratorDlg</name>
-    <message>
-        <source>ICON_SELECT</source>
-        <translation>select1.png</translation>
-    </message>
-</context>
-</TS>

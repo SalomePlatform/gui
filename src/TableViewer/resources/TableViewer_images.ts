37126830affc7c17e876e57e3fea9b593edac4dd
nds <nds@opencascade.com>
2007-10-02 07:50:44 +0000

    Resources improving.

--- src/TableViewer/resources/TableViewer_images.ts
+++ src/TableViewer/resources/TableViewer_images.ts
@@ -1,24 +1,13 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="ru_RU">
+<!DOCTYPE TS><TS>
 <context>
-    <name>TableViewer_ViewManager</name>
+    <name>@default</name>
     <message>
-        <location filename="TableViewer_ViewManager.cxx" line="32"/>
-        <source>TABLE_VIEWER_VIEW_TITLE</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>TableViewer_ViewWindow</name>
-    <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="100"/>
         <source>ICON_DUMP</source>
-        <translation type="unfinished">table_camera_dump.png</translation>
+        <translation>table_camera_dump.png</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="112"/>
         <source>ICON_EXPORT</source>
-        <translation type="unfinished">table_export.png</translation>
+        <translation>table_export.png</translation>
     </message>
 </context>
 </TS>
--- src/TableViewer/resources/TableViewer_msg_en.ts
+++ src/TableViewer/resources/TableViewer_msg_en.ts
@@ -1,89 +1,81 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="ru_RU">
+<!DOCTYPE TS><TS>
 <context>
-    <name>TableViewer_ViewManager</name>
+    <name>@default</name>
     <message>
-        <location filename="TableViewer_ViewManager.cxx" line="32"/>
         <source>TABLE_VIEWER_VIEW_TITLE</source>
-        <translation type="unfinished">TableViewer scene:%M - viewer %V</translation>
+        <translation>TableViewer scene:%M - viewer %V</translation>
     </message>
-</context>
-<context>
-    <name>TableViewer_ViewWindow</name>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="55"/>
         <source>LBL_TOOLBAR_LABEL</source>
-        <translation type="unfinished">View operations</translation>
+        <translation>View operations</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="99"/>
         <source>MEN_DUMP</source>
-        <translation type="unfinished">Dump view...</translation>
+        <translation>Dump view...</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="101"/>
         <source>TIP_DUMP</source>
-        <translation type="unfinished">Dump view</translation>
+        <translation>Dump view</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="101"/>
         <source>PRP_DUMP</source>
-        <translation type="unfinished">Dump view</translation>
+        <translation>Dump view</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="103"/>
         <source>MEN_COPY</source>
-        <translation type="unfinished">Copy</translation>
+        <translation>Copy</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="105"/>
         <source>TIP_COPY</source>
-        <translation type="unfinished">Copy</translation>
+        <translation>Copy</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="105"/>
         <source>PRP_COPY</source>
-        <translation type="unfinished">Copy</translation>
+        <translation>Copy</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="107"/>
+        <source>MEN_PASTE</source>
+        <translation>Paste</translation>
+    </message>
+    <message>
+        <source>TIP_PASTE</source>
+        <translation>Paste</translation>
+    </message>
+    <message>
+        <source>PRP_PASTE</source>
+        <translation>Paste</translation>
+    </message>
+    <message>
         <source>MEN_PRINT</source>
-        <translation type="unfinished">Print view</translation>
+        <translation>Print view</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="109"/>
         <source>TIP_PRINT</source>
-        <translation type="unfinished">Print view</translation>
+        <translation>Print view</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="109"/>
         <source>PRP_PRINT</source>
-        <translation type="unfinished">Print view</translation>
+        <translation>Print view</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="111"/>
         <source>MEN_EXPORT</source>
-        <translation type="unfinished">Export to HTML...</translation>
+        <translation>Export to HTML...</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="113"/>
         <source>TIP_EXPORT</source>
-        <translation type="unfinished">Export as HTML file</translation>
+        <translation>Export as HTML file</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="113"/>
         <source>PRP_EXPORT</source>
-        <translation type="unfinished">Export to HTML file</translation>
+        <translation>Export to HTML file</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="296"/>
         <source>EXPORT_FILTER</source>
-        <translation type="unfinished">Hyper text document (*.htm *.html)</translation>
+        <translation>Hyper text document (*.htm *.html)</translation>
     </message>
     <message>
-        <location filename="TableViewer_ViewWindow.cxx" line="297"/>
         <source>EXPORT_TABLE</source>
-        <translation type="unfinished">Export table to file</translation>
+        <translation>Export table to file</translation>
     </message>
 </context>
 </TS>

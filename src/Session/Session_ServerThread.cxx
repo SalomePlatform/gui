fefa4c200fde763755d8c483b068de217510ab97
abd <abd@opencascade.com>
2008-11-26 14:21:59 +0000

    Update copyright information

--- src/Session/Session_ServerThread.cxx
+++ src/Session/Session_ServerThread.cxx
@@ -1,6 +1,6 @@
-//  SALOME Session : implementation of Session_ServerThread.cxx
+//  Copyright (C) 2007-2008  CEA/DEN, EDF R&D, OPEN CASCADE
 //
-//  Copyright (C) 2003  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+//  Copyright (C) 2003-2007  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
 //  CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
 //
 //  This library is free software; you can redistribute it and/or
@@ -17,19 +17,17 @@
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 //
-// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 //
-//
-//
+//  SALOME Session : implementation of Session_ServerThread.cxx
 //  File   : Session_ServerThread.cxx
 //  Author : Paul RASCLE, EDF
 //  Module : SALOME
 //  $Header$
-
 // #include <SALOMEconfig.h>
 // #include CORBA_SERVER_HEADER(SALOME_Session)
 // #include CORBA_SERVER_HEADER(SALOMEDS)
-
+//
 #include "Session_ServerThread.hxx"
 
 #include "SALOME_Container_i.hxx"

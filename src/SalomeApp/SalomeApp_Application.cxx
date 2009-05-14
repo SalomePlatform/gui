48723c1fa14b5bdf0ef66f6ea66fd4a48c881ee5
vsr <vsr@opencascade.com>
2009-05-14 13:10:13 +0000

    Rollback previous integration

--- src/SalomeApp/SalomeApp_Application.cxx
+++ src/SalomeApp/SalomeApp_Application.cxx
@@ -765,10 +765,7 @@ void SalomeApp_Application::onLoadScript( )
 
   if ( !aFile.isEmpty() )
   {
-    QFileInfo aFileInfo( aFile );
-    QString anAbsPath = aFileInfo.absolutePath();
-    QString aFileName = aFileInfo.baseName();
-    QString command = QString("import sys; sys.path.insert(0,r\"%1\"); import %2").arg(anAbsPath, aFileName);
+    QString command = QString("execfile(r\"%1\")").arg(aFile);
 
     PyConsole_Console* pyConsole = pythonConsole();
 

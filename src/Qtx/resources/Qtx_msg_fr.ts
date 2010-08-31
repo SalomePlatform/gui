df12cc1d9a0731a96c4c2c7133aec8f108c72b1e
vsr <vsr@opencascade.com>
2010-08-31 14:35:26 +0000

    SALOME internationalization: add textual resources for French language

--- src/Qtx/resources/Qtx_msg_fr.ts
+++ src/Qtx/resources/Qtx_msg_fr.ts
@@ -5,100 +5,100 @@
     <name>QtxTreeView</name>
     <message>
         <source>Enable sorting</source>
-        <translation type="unfinished"></translation>
+        <translation>Activer le triage</translation>
     </message>
 </context>
 <context>
     <name>QtxWorkspaceAction</name>
     <message>
         <source>Arranges the windows as overlapping tiles</source>
-        <translation type="unfinished"></translation>
+        <translation>Ranger les fenêtres en pavés superposés</translation>
     </message>
     <message>
         <source>Cascade</source>
-        <translation type="unfinished"></translation>
+        <translation>Cascade</translation>
     </message>
     <message>
         <source>Arranges the windows as nonoverlapping tiles</source>
-        <translation type="unfinished"></translation>
+        <translation>Ranger les fenêtres en pavés sans superposition</translation>
     </message>
     <message>
         <source>Tile</source>
-        <translation type="unfinished"></translation>
+        <translation>Pavé</translation>
     </message>
     <message>
         <source>Arranges the windows as nonoverlapping horizontal tiles</source>
-        <translation type="unfinished"></translation>
+        <translation>Ranger les fenêtres en pavés à l&apos;horisontale et sans superposition</translation>
     </message>
     <message>
         <source>Tile horizontally</source>
-        <translation type="unfinished"></translation>
+        <translation>Ranger à l&apos;horisontale</translation>
     </message>
     <message>
         <source>Arranges the windows as nonoverlapping vertical tiles</source>
-        <translation type="unfinished"></translation>
+        <translation>Ranger les fenêtres en pavés à la verticale et sans superposition</translation>
     </message>
     <message>
         <source>Tile vertically</source>
-        <translation type="unfinished"></translation>
+        <translation>Ranger au verticale</translation>
     </message>
 </context>
 <context>
     <name>QtxMainWindow</name>
     <message>
         <source>Menu bar</source>
-        <translation type="unfinished"></translation>
+        <translation>Barre de menu</translation>
     </message>
     <message>
         <source>Status bar</source>
-        <translation type="unfinished"></translation>
+        <translation>Barre de statut</translation>
     </message>
 </context>
 <context>
     <name>QtxPathDialog</name>
     <message>
         <source>Open file</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir le fichier</translation>
     </message>
     <message>
         <source>Save file</source>
-        <translation type="unfinished"></translation>
+        <translation>Sauvegarder le fichier</translation>
     </message>
     <message>
         <source>File name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom du fichier</translation>
     </message>
     <message>
         <source>File name not specified</source>
-        <translation type="unfinished"></translation>
+        <translation>Le nom du fichier n&apos;est pas indiqué</translation>
     </message>
     <message>
         <source>File \&quot;%1\&quot; does not exist</source>
-        <translation type="unfinished"></translation>
+        <translation>Le fichier \&quot;%1\&quot; n&apos;existe pas</translation>
     </message>
     <message>
         <source>File \&quot;%1\&quot; already exist. Do you want to overwrite it?</source>
-        <translation type="unfinished"></translation>
+        <translation>Le fichier \&quot;%1\&quot; déjà existe. Voulez-vous le réécrire?</translation>
     </message>
     <message>
         <source>Directory \&quot;%1\&quot; does not exist</source>
-        <translation type="unfinished"></translation>
+        <translation>Le répertoire \&quot;%1\&quot; n&apos;existe pas</translation>
     </message>
     <message>
         <source>Directory \&quot;%1\&quot; can&apos;t be created because file with the same name exist</source>
-        <translation type="unfinished"></translation>
+        <translation>Il est impossible de créer le répertoire \&quot;%1\&quot; parce qu&apos;un fichier avec ce nom déjà existe</translation>
     </message>
     <message>
         <source>Directory \&quot;%1\&quot; not empty. Do you want to remove all files in this directory?</source>
-        <translation type="unfinished"></translation>
+        <translation>Le répertoire \&quot;%1\&quot; n&apos;est pas vide. Voulez-vous supprimer tous les fichier dans ce répertoire?</translation>
     </message>
     <message>
         <source>File dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Dialogue de fichier</translation>
     </message>
     <message>
         <source>All files (*.*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tous les fichiers (*.*)</translation>
     </message>
 </context>
 <context>
@@ -106,181 +106,181 @@
     <message>
         <source>B</source>
         <translatorcomment>Bold</translatorcomment>
-        <translation type="unfinished"></translation>
+        <translation>G</translation>
     </message>
     <message>
         <source>I</source>
         <translatorcomment>Italic</translatorcomment>
-        <translation type="unfinished"></translation>
+        <translation>I</translation>
     </message>
     <message>
         <source>U</source>
         <translatorcomment>Underline</translatorcomment>
-        <translation type="unfinished"></translation>
+        <translation>S</translation>
     </message>
     <message>
         <source>S</source>
         <translatorcomment>Strikethrough</translatorcomment>
-        <translation type="unfinished"></translation>
+        <translation>R</translation>
     </message>
 </context>
 <context>
     <name>QtxPathListEdit</name>
     <message>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Avertissement</translation>
     </message>
     <message>
         <source>Path \&quot;%1\&quot; doesn&apos;t exist. Add it to list anyway?</source>
-        <translation type="unfinished"></translation>
+        <translation>Le chemin \&quot;%1\&quot; n&apos;existe pas. Faut-il l&apos;ajouter dans la liste?</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur</translation>
     </message>
     <message>
         <source>Location \&quot;%1\&quot; doesn&apos;t point to file</source>
-        <translation type="unfinished"></translation>
+        <translation>La location \&quot;%1\&quot; ne contient pas d&apos;indication du fichier</translation>
     </message>
     <message>
         <source>Location \&quot;%1\&quot; doesn&apos;t point to directory</source>
-        <translation type="unfinished"></translation>
+        <translation>La location \&quot;%1\&quot; ne contient pas d&apos;indication du répertoire</translation>
     </message>
     <message>
         <source>Path \&quot;%1\&quot; already exist in the list</source>
-        <translation type="unfinished"></translation>
+        <translation>Le chemin \&quot;%1\&quot; déjà existe dans la liste</translation>
     </message>
 </context>
 <context>
     <name>QtxMRUAction</name>
     <message>
         <source>Most Recently Used</source>
-        <translation type="unfinished"></translation>
+        <translation>Récemment utilisés</translation>
     </message>
     <message>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>Effacer</translation>
     </message>
     <message>
         <source>&lt;Empty&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;Vide&gt;</translation>
     </message>
 </context>
 <context>
     <name>QtxDialog</name>
     <message>
         <source>&amp;OK</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;OK</translation>
     </message>
     <message>
         <source>&amp;Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Annuler</translation>
     </message>
     <message>
         <source>C&amp;lose</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Fermer</translation>
     </message>
     <message>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Aide</translation>
     </message>
     <message>
         <source>&amp;Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>A&amp;ppliquer</translation>
     </message>
     <message>
         <source>&amp;Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Oui</translation>
     </message>
     <message>
         <source>&amp;No</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Non</translation>
     </message>
 </context>
 <context>
     <name>QtxSearchTool</name>
     <message>
         <source>Case sensitive</source>
-        <translation type="unfinished"></translation>
+        <translation>Sensible à la casse</translation>
     </message>
     <message>
         <source>Regular expression</source>
-        <translation type="unfinished"></translation>
+        <translation>Expression régulaire</translation>
     </message>
     <message>
         <source>Wrap search</source>
-        <translation type="unfinished"></translation>
+        <translation>Terminer la recherche</translation>
     </message>
 </context>
 <context>
     <name>QtxSplash</name>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur</translation>
     </message>
     <message>
         <source>&amp;OK</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;OK</translation>
     </message>
 </context>
 <context>
     <name>QtxWorkstack</name>
     <message>
         <source>Split vertically</source>
-        <translation type="unfinished"></translation>
+        <translation>Diviser verticalement</translation>
     </message>
     <message>
         <source>Split horizontally</source>
-        <translation type="unfinished"></translation>
+        <translation>Diviser horisontalement</translation>
     </message>
     <message>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Fermer</translation>
     </message>
     <message>
         <source>Rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Renommer</translation>
     </message>
     <message>
         <source>Enter new name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Indiquer un nouveau nom</translation>
     </message>
 </context>
 <context>
     <name>QtxWorkstackAction</name>
     <message>
         <source>Split the active window on two vertical parts</source>
-        <translation type="unfinished"></translation>
+        <translation>Diviser la fenêtre actuelle en deux parties verticales</translation>
     </message>
     <message>
         <source>Split vertically</source>
-        <translation type="unfinished"></translation>
+        <translation>Diviser à la verticale</translation>
     </message>
     <message>
         <source>Split the active window on two horizontal parts</source>
-        <translation type="unfinished"></translation>
+        <translation>Diviser la fenêtre actuelle en deux parties horisontales</translation>
     </message>
     <message>
         <source>Split horizontally</source>
-        <translation type="unfinished"></translation>
+        <translation>Diviser à l&apos;horisontale</translation>
     </message>
 </context>
 <context>
     <name>QtxColorButton</name>
     <message>
         <source>Auto</source>
-        <translation type="unfinished"></translation>
+        <translation>Auto</translation>
     </message>
     <message>
         <source>Other colors...</source>
-        <translation type="unfinished"></translation>
+        <translation>Autres couleurs...</translation>
     </message>
 </context>
 <context>
     <name>QtxColorScale</name>
     <message>
         <source>Color scale</source>
-        <translation type="unfinished"></translation>
+        <translation>Echelle de couleurs</translation>
     </message>
 </context>
 </TS>

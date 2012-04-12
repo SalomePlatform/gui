354679c5d8539bbc91c3d0e5d87c6381d05348c1
vsr <vsr@opencascade.com>
2012-04-12 15:52:12 +0000

    Update copyright information

--- tools/vtkEDFOverloads/vtkEDFFactory.cxx
+++ tools/vtkEDFOverloads/vtkEDFFactory.cxx
@@ -1,21 +1,24 @@
-/*=========================================================================
+// Copyright (C) 2010-2012  CEA/DEN, EDF R&D, OPEN CASCADE
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
 
-  Program:   Visualization Toolkit
-  Module:    vtkEDFFactory.cxx
-
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
-
-=========================================================================*/
 #include "vtkEDFFactory.h"
-
 #include "vtkVersion.h"
-
 #include "vtkEDFCutter.h"
 
 vtkStandardNewMacro(vtkEDFFactory);
